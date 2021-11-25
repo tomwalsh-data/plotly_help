498c76514c934897b1adf11f8f2a4389d8c95fe9
Thomas Walsh <walsh@roselandpc.com>
2021-11-25 12:19:58 +0000

    updated callback to get required functionallity

--- iris_example.py
+++ iris_example.py
@@ -20,9 +20,8 @@ app.layout = dbc.Container([
         dbc.Col([
             html.H1("Update Selected Points"),
             html.H4("Minimal working example--Iris"),
-            html.H6("""Choosing points from the dropdown selects those points
-for each species.\n Intended behaviour is to select only those points irrespective of species. \n 
-e.g. point 0 should select only the first flower, but currently selects the first of each species"""),
+            html.H6("""Fixed! Selecting point from the dropdown now selected only that point
+rather than that point for each species :)"""),
             html.Hr()
             ])
         ]),
@@ -31,7 +30,7 @@ e.g. point 0 should select only the first flower, but currently selects the firs
             # similified dropdown to select df indices
             dcc.Dropdown(
                 id='iris-dropdown',
-                options = [{'label': x, 'value': x} for x in range(10)],
+                options = [{'label': x, 'value': x} for x in range(len(df))],
                 multi=True,
                 placeholder="Select point indices"                
                 ),
@@ -63,30 +62,32 @@ def update_plot(fig_old, selection):
     
     """
 
-    # read in the old figure for modification--"Don't Change" the zoom/pan on update
-    fig = go.Figure(fig_old)
-    fig.update_layout(uirevision="Don't Change")
-
+    # select all points were selection is empty/unspecified
     if (selection is None) or (len(selection) == 0):
-        # no points are selected: highlight all
         points = [i for i in range(len(df))]
+        fig = go.Figure(fig_old)
+        fig.update_traces(selectedpoints=points)
 
+    # select only the intended points
     else:
-        # choose the selected points--more complicated in real app
-        points = selection
 
-    # update `selectedpoints`
-    fig.update_traces(selectedpoints=points)
+        # extract esch groups dict from the figure dict
+        groups = [i for i in fig_old['data']]
+        k = None
 
-    """
-    current result: selects the passed indices for each species
-                    e.g. pass [0,1,2] currently selects the first three points for each species
-                    -> 9 points highlighted (first 3 points for each of 3 species)
+        # iterate over groups (species)
+        for gi, group in enumerate(groups):
+            xi = [i for i in range(len(group['x']))]             # point indices for the group (using x-coords)
+            k = xi if k is None else [i + k[-1] + 1 for i in xi] # absolute point indices
 
-    desired result: select the passed indices from the dataframe only:
-                    e.g. pass [0,1,2] to select the first three flowers, regardless of species
-                    -> 3 points highlighted, color irrelevent.
-    """
+            # select group indices where corresponding absolute indices are selected
+            group_points = [xi[i] for i, p in enumerate(k) if p in selection]  
+            fig_old['data'][gi]['selectedpoints'] = group_points # update the group dict
+
+        # read the dict as plotly figure for futher use
+        fig = go.Figure(fig_old)
+
+    fig.update_layout(uirevision="Don't Change")
 
     return fig
     
